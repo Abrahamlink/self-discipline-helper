80cb9e61a94b81fac897f4795991e32b6764f6e7
Berkut <brekutsgaming@mail.ru>
2022-03-06 23:42:36 +0500

    addition

--- self-discipline/src/settings.py
+++ self-discipline/src/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+zvkh@p%pv3zi51+$7*0p53_)69gg5zsy8_2$cqegv@6fqtk1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'discipline-manager.herokuapp.com']
 
 
 # Application definition
